9b59efc71db732c941da566575ba546d9aa1367a
SantiagoChevez <a01749887@tec.mx>
2026-08-19 20:27:38 -0600

    V2 projects/language

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://santiagochevez.github.io',[cite: 1]
+  site: 'https://santiagochevez.github.io',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'en',
